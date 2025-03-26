916a4a72b0be15bdae30982048f2085962bce794
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-03-26 14:20:22 +0100

    add mobile menu toggle functionality

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 // Select DOM elements
 document.addEventListener("DOMContentLoaded", function () {
   // Get all necessary DOM elements
+  const openMobileMenu = document.getElementById("open-menu");
+  const closeMobileMenu = document.getElementById("close-menu");
+  const mobileNav = document.getElementById("mobile-nav");
+  const overlay = document.getElementById("overlay");
+
+  // Toggle mobile menu and overlay
+  function toggleMenu() {
+    const isMenuOpen = mobileNav.classList.contains("d-flex");
+
+    if (isMenuOpen) {
+      // Close the menu
+      mobileNav.classList.remove("d-flex");
+      mobileNav.classList.add("d-none");
+      overlay.classList.remove("d-flex");
+      overlay.classList.add("d-none");
+    } else {
+      // Open the menu
+      mobileNav.classList.remove("d-none");
+      mobileNav.classList.add("d-flex");
+      overlay.classList.remove("d-none");
+      overlay.classList.add("d-flex");
+    }
+  }
+
+  // Add event listeners
+  openMobileMenu.addEventListener("click", toggleMenu);
+  closeMobileMenu.addEventListener("click", toggleMenu);
+  overlay.addEventListener("click", toggleMenu); // Close menu when overlay is clicked
+
+  //   Slider
   const slides = document.querySelectorAll(".slide");
   const leftBtn = document.getElementById("left-btn");
   const rightBtn = document.getElementById("right-btn");
